185a3b698e0aa6dfec31383d6efd7d972ee2549f
Chu Tat Hon <chutathon@yahoo.com>
2011-01-25 19:04:20 -0800

    Done with sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -11,4 +11,28 @@ describe "LayoutLinks" do
 		click_link "Home"
 		response.should have_selector('title', :content => "Home")
 	end
+	
+	describe "when not signed in" do
+		it "should have a signin link" do
+			visit root_path
+			response.should have_selector("a", 	:href => signin_path,
+																					:content => "Sign in")
+		end
+	end
+	
+	describe "when signin in" do
+		before(:each) do
+			@user = Factory(:user)
+			visit signin_path
+			fill_in :userid,		:with => @user.userid
+			fill_in :password,	:with => @user.password
+			click_button
+		end
+		
+		it "should have a signout link" do
+			visit root_path
+			response.should have_selector("a",	:href => signout_path,
+																					:content => "Sign out")
+		end		
+	end
 end
