075780286525f9e095ba3546944be7b6b5bb7678
Chu Tat Hon <chutathon@yahoo.com>
2011-01-25 20:15:48 -0800

    Added report selection to Home page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,5 +14,30 @@ describe PagesController do
     	response.should have_selector("title", :content => "Management Portal | Home")
     end
   end
+  
+  describe "authentication of Management Reports pages" do
+  	before(:each) do
+  		@user = Factory(:user)
+  	end
+  	
+  	describe "for non-signed-in users" do
+  		it "should not have link to 'PS451'" do
+  			get :home
+				response.should_not have_selector("a",
+																					:href => "#",
+																					:content => "PS451")  		
+  		end
+  	end
+  	
+  	describe "for signed-in users" do
+			it "should have link to 'PS451'" do
+				test_sign_in(@user)
+				get :home
+				response.should have_selector("a",
+																			:href => "#",
+																			:content => "PS451")  		
+			end
+  	end
+  end
 
 end
